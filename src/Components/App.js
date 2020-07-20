bae20345fe6b7190ce8015db5ac9c639a5965fb2
Jeffrey <jeffrey.liu90@gmail.com>
2020-07-20 01:35:49 -0400

    created the board and squares

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Board from "./Board";
+
+import "../styles.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div></div>
+      <Board></Board>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Board.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import Tiles from "./Tiles";
+import "../styles.css";
+
+function Board() {
+  //state
+  //boardState
+
+  const tilePositions = array => {
+    const arr = [];
+    array.map(letter => {
+      for (let i = 1; i <= 10; i++) {
+        arr.push(letter + i);
+      }
+    });
+    return arr;
+  };
+
+  const boardPositions = tilePositions([
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J"
+  ]);
+
+  const [boardSquares, setBoardSquares] = useState(boardPositions);
+
+  const handleClick = () => {
+    console.log("hit");
+  };
+
+  const renderSquare = index => {
+    return (
+      <Tiles
+        value={boardSquares[index]}
+        onClick={() => handleClick(index)}
+      ></Tiles>
+    );
+  };
+
+  console.log(
+    "func",
+    boardSquares.map((item, index) => item + " index: " + index)
+  );
+
+  return (
+    <div>
+      <h3>BattleShip Game</h3>
+      <div className="board-row">
+        {boardSquares.slice(0, 10).map((item, index) => {
+          return <div key={item}>{renderSquare(index)}</div>;
+        })}
+      </div>
+      <div className="board-row">
+        {boardSquares.slice(10, 20).map((item, index) => {
+          return <div key={item}>{renderSquare(index + 10)}</div>;
+        })}
+      </div>
+      <div className="board-row">
+        {boardSquares.slice(20, 30).map((item, index) => {
+          return <div key={item}>{renderSquare(index + 20)}</div>;
+        })}
+      </div>
+      <div className="board-row">
+        {boardSquares.slice(30, 40).map((item, index) => {
+          return <div key={item}>{renderSquare(index + 30)}</div>;
+        })}
+      </div>
+      <div className="board-row">
+        {boardSquares.slice(40, 50).map((item, index) => {
+          return <div key={item}>{renderSquare(index + 40)}</div>;
+        })}
+      </div>
+      <div className="board-row">
+        {boardSquares.slice(50, 60).map((item, index) => {
+          return <div key={item}>{renderSquare(index + 50)}</div>;
+        })}
+      </div>
+      <div className="board-row">
+        {boardSquares.slice(60, 70).map((item, index, key) => {
+          return <div key={item}>{renderSquare(index + 60)}</div>;
+        })}
+      </div>
+      <div className="board-row">
+        {boardSquares.slice(70, 80).map((item, index) => {
+          return <div key={item}>{renderSquare(index + 70)}</div>;
+        })}
+      </div>
+      <div className="board-row">
+        {boardSquares.slice(80, 90).map((item, index) => {
+          return <div key={item}>{renderSquare(index + 80)}</div>;
+        })}
+      </div>
+      <div className="board-row">
+        {boardSquares.slice(90, 100).map((item, index) => {
+          return <div key={item}>{renderSquare(index + 90)}</div>;
+        })}
+      </div>
+
+      <div className="board-row"></div>
+      <div className="board-row"></div>
+    </div>
+  );
+}
+
+export default Board;
