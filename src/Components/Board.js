3ec33669c99d3ccc8b433d04d000c93f35792fcd
Jeffrey <jeffrey.liu90@gmail.com>
2020-07-21 14:51:57 -0400

    Update Board.js

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -30,72 +30,91 @@ function Board() {
   ]);
 
   const [boardSquares, setBoardSquares] = useState(boardPositions);
+  const [gameStatus, setGameStatus] = useState("setup");
 
-  const handleClick = index => {
+  const handleHit = index => {
     console.log("hit");
     const squares = [...boardSquares];
     let currentIndex = squares.indexOf(squares[index]);
     console.log(currentIndex);
     squares[currentIndex] = "hit";
     setBoardSquares(squares);
-    // console.log("hey", squares.indexOf(squares[index]));
+  };
+
+  const resetGame = () => {
+    setGameStatus("setup");
+    setBoardSquares(boardPositions);
+  };
+
+  const handleSetShip = index => {
+    console.log("yo");
+    const squares = [...boardSquares];
+    let currentIndex = squares.indexOf(squares[index]);
+    console.log("current", currentIndex);
+    squares[currentIndex] = "ship";
+    setBoardSquares(squares);
   };
 
   const renderSquare = index => {
     return (
       <Tiles
         value={boardSquares[index]}
-        onClick={() => handleClick(index)}
+        onClick={() =>
+          gameStatus === "setup" ? handleSetShip(index) : handleHit(index)
+        }
       ></Tiles>
     );
   };
 
+  const startGame = () => {
+    setGameStatus("started");
+  };
   return (
     <div>
       <h3>BattleShip Game</h3>
       <div className="board-row">
         {boardSquares.slice(0, 10).map((item, index) => {
-          return <div key={item}>{renderSquare(index)}</div>;
+          return <div key={index}>{renderSquare(index)}</div>;
         })}
       </div>
       <div className="board-row">
         {boardSquares.slice(10, 20).map((item, index) => {
-          return <div key={item}>{renderSquare(index + 10)}</div>;
+          return <div key={index}>{renderSquare(index + 10)}</div>;
         })}
       </div>
       <div className="board-row">
         {boardSquares.slice(20, 30).map((item, index) => {
-          return <div key={item}>{renderSquare(index + 20)}</div>;
+          return <div key={index}>{renderSquare(index + 20)}</div>;
         })}
       </div>
       <div className="board-row">
         {boardSquares.slice(30, 40).map((item, index) => {
-          return <div key={item}>{renderSquare(index + 30)}</div>;
+          return <div key={index}>{renderSquare(index + 30)}</div>;
         })}
       </div>
       <div className="board-row">
         {boardSquares.slice(40, 50).map((item, index) => {
-          return <div key={item}>{renderSquare(index + 40)}</div>;
+          return <div key={index}>{renderSquare(index + 40)}</div>;
         })}
       </div>
       <div className="board-row">
         {boardSquares.slice(50, 60).map((item, index) => {
-          return <div key={item}>{renderSquare(index + 50)}</div>;
+          return <div key={index}>{renderSquare(index + 50)}</div>;
         })}
       </div>
       <div className="board-row">
-        {boardSquares.slice(60, 70).map((item, index, key) => {
-          return <div key={item}>{renderSquare(index + 60)}</div>;
+        {boardSquares.slice(60, 70).map((item, index) => {
+          return <div key={index}>{renderSquare(index + 60)}</div>;
         })}
       </div>
       <div className="board-row">
         {boardSquares.slice(70, 80).map((item, index) => {
-          return <div key={item}>{renderSquare(index + 70)}</div>;
+          return <div key={index}>{renderSquare(index + 70)}</div>;
         })}
       </div>
       <div className="board-row">
         {boardSquares.slice(80, 90).map((item, index) => {
-          return <div key={item}>{renderSquare(index + 80)}</div>;
+          return <div key={index}>{renderSquare(index + 80)}</div>;
         })}
       </div>
       <div className="board-row">
@@ -103,9 +122,15 @@ function Board() {
           return <div key={item}>{renderSquare(index + 90)}</div>;
         })}
       </div>
-
-      <div className="board-row"></div>
-      <div className="board-row"></div>
+      <button onClick={() => startGame()}>Click to Start Game </button>
+      <button onClick={() => resetGame()}>Click to Reset the Game </button>
+      <form>
+        <label>
+          Ship Position
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
